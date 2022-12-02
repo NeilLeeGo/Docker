8fb3c805b70cc4ecfde8769dbd1c130335c6fcbd
neil <neil.lee@adlinktech.com>
2022-12-02 11:23:55 +0800

    Nvidia: Split the docker file as two for L4T-32 and L4T-35.

--- nvidia/build.sh
+++ nvidia/build.sh
@@ -10,16 +10,16 @@ CYAN='\033[0;36m'         # Cyan
 WHITE='\033[0;37m'        # White
 NC='\033[0m'
 
-while [ -z ${OS} ]
+while [ -z ${L4T} ]
 do
-	echo -e "${OS}Choose container Ubuntu version: 1 or 2, (1:18.04 , 2: 20.04):${NC}"
-	read OS
-	case ${OS} in
-		1) OS=ubuntu:18.04
+	echo -e "${L4T}Choose L4T version: 1 or 2, (1:L4T-32 , 2: L4T-35):${NC}"
+	read L4T
+	case ${L4T} in
+		1) L4T=l4t32
 			;;
-		2) OS=ubuntu:20.04
+		2) L4T=l4t35
 			;;
-		*) OS=
+		*) L4T=
 			;;
 	esac
 done
@@ -55,7 +55,7 @@ sleep 3
 RUN='./run.sh'
 echo '# !/bin/bash' > ${RUN}
 echo '' >> ${RUN}
-echo 'docker run --privileged -it -v /dev/bus/usb:/dev/bus/usb -v /etc/udev:/etc/udev -v '${WORKDIR}':/home/'${NAME}/Workspace' nvidia:latest' >> ${RUN}
+echo 'docker run --privileged -it -v /dev/bus/usb:/dev/bus/usb -v /etc/udev:/etc/udev -v '${WORKDIR}':/home/'${NAME}/Workspace' nvidia:'${L4T} >> ${RUN}
 chmod +x ${RUN}
 
-docker build --build-arg OS=${OS} --build-arg NAME=${NAME} --build-arg PASSWD=${PASSWD} -t nvidia -f nvidia.docker ./
+docker build --build-arg NAME=${NAME} --build-arg PASSWD=${PASSWD} -t nvidia:${L4T} -f ${L4T}.docker ./
